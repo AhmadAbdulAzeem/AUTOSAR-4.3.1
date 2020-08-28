500dc8bed5c56f9025c0df489e49d781aec8b62a
Ahmed AbdulAzeem <54190264+AhmadAbdulAzeem@users.noreply.github.com>
2020-08-28 14:30:10 +0200

    Create Port_Cfg.h

---
+++ Port/Port_Cfg.h
@@ -0,0 +1,13 @@
+#ifndef PORT_CFG_H
+#define POET_CFG_H
+
+#define GPIO_PIN_0						(0u)
+#define GPIO_PIN_1						(1u)
+#define GPIO_PIN_2						(2u)
+#define GPIO_PIN_3						(3u)
+#define GPIO_PIN_4						(4u)
+#define GPIO_PIN_5						(5u)
+#define GPIO_PIN_6						(6u)
+#define GPIO_PIN_7						(7u)
+
+#endif	
