44a09cdd12bbd5c918c30c5e66e7ab7cc06082ee
Ahmed AbdulAzeem <54190264+AhmadAbdulAzeem@users.noreply.github.com>
2020-09-02 23:50:57 +0200

    Add files via upload

---
+++ RTE Tasks/SeatCtrlApp_Task2/HMI_Template.c
@@ -0,0 +1,46 @@
+/**
+ *
+ * \file HMI_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: HMI
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 9/2/2020 11:16 PM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_HMI.h"
+
+
+/**
+ *
+ * Runnable HMI_MainFunction
+ *
+ * Triggered By:
+ *  - TimingEventImpl.TE_HMI_MainFunction_100ms
+ *
+ */
+
+void HMI_MainFunction (void)
+{
+	Std_ReturnType status;
+	MultiStateBtnType HeightBtnState;
+	MultiStateBtnType InclineBtnState;
+	MultiStateBtnType SlideBtnState;
+	uint8 Height;
+	uint8 Inclne;
+	uint8 Slide;
+
+	/* Data Send Points */
+	status = Rte_Write_ppSeatCtrlBtns_HeightBtnState(HeightBtnState);
+	status = Rte_Write_ppSeatCtrlBtns_InclineBtnState(InclineBtnState);
+	status = Rte_Write_ppSeatCtrlBtns_SlideBtnState(SlideBtnState);
+	
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlData_Height(&Height);
+	status = Rte_Read_rpSeatCtrlData_Inclne(&Inclne);
+	status = Rte_Read_rpSeatCtrlData_Slide(&Slide);
+	
+}
+
---
+++ RTE Tasks/SeatCtrlApp_Task2/src/SeatManager.c
@@ -0,0 +1,74 @@
+/**
+ *
+ * \file SeatManager_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: SeatManager
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 9/2/2020 11:16 PM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_SeatManager.h"
+
+
+/**
+ *
+ * Runnable SeatManager_SetHeight
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_HeightBtnState
+ *
+ */
+
+void SeatManager_SetHeight (void)
+{
+	Std_ReturnType status;
+	MultiStateBtnType HeightBtnState;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_HeightBtnState(&HeightBtnState);
+	
+}
+
+
+/**
+ *
+ * Runnable SeatManager_SetIncline
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_InclineBtnState
+ *
+ */
+
+void SeatManager_SetIncline (void)
+{
+	Std_ReturnType status;
+	MultiStateBtnType InclineBtnState;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_InclineBtnState(&InclineBtnState);
+	
+}
+
+
+/**
+ *
+ * Runnable SeatManager_SetSlide
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_SlideBtnState
+ *
+ */
+
+void SeatManager_SetSlide (void)
+{
+	Std_ReturnType status;
+	MultiStateBtnType SlideBtnState;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_SlideBtnState(&SlideBtnState);
+	
+}
+
