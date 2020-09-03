86d62a5f2ee58da63b34f76a912eed3f60abb0fb
Ahmed AbdulAzeem <54190264+AhmadAbdulAzeem@users.noreply.github.com>
2020-09-03 08:37:50 +0200

    Add files via upload

---
+++ RTE Tasks/SeatCtrlApp_Task3/src/SeatManager.c
@@ -0,0 +1,98 @@
+/**
+ *
+ * \file SeatManager_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: SeatManager
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 9/3/2020 08:24 AM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_SeatManager.h"
+
+
+/**
+ *
+ * Runnable SeatManager_SetHeight
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_HeightBtnState
+ *
+ */
+
+void SeatManager_SetHeight (void)
+{
+	Std_ReturnType status;
+	MultiStateBtnType HeightBtnState;
+	StepMotorStepType step;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_HeightBtnState(&HeightBtnState);
+	
+	if(HeightBtnState == MULTI_STATE_BTN_MINUS)
+		status = Rte_Call_rpHeightMotor_Move(MOTOR_STEP_MINUS);
+	
+	if(HeightBtnState == MULTI_STATE_BTN_PLUS)
+		status = Rte_Call_rpHeightMotor_Move(MOTOR_STEP_PLUS);
+	
+	
+}
+
+
+/**
+ *
+ * Runnable SeatManager_SetIncline
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_InclineBtnState
+ *
+ */
+
+void SeatManager_SetIncline (void)
+{
+	Std_ReturnType status;
+	MultiStateBtnType InclineBtnState;
+	StepMotorStepType step;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_InclineBtnState(&InclineBtnState);
+	
+	if(InclineBtnState == MULTI_STATE_BTN_MINUS)
+		status = Rte_Call_rpInclineMotor_Move(MOTOR_STEP_MINUS);
+	
+	if(InclineBtnState == MULTI_STATE_BTN_PLUS)
+		status = Rte_Call_rpInclineMotor_Move(MOTOR_STEP_PLUS);
+	
+	
+}
+
+
+/**
+ *
+ * Runnable SeatManager_SetSlide
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_SlideBtnState
+ *
+ */
+
+void SeatManager_SetSlide (void)
+{
+	Std_ReturnType status;
+	MultiStateBtnType SlideBtnState;
+	StepMotorStepType step;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_SlideBtnState(&SlideBtnState);
+	
+	if(SlideBtnState == MULTI_STATE_BTN_MINUS)
+		status = Rte_Call_rpSlideMotor_Move(MOTOR_STEP_MINUS);
+	
+	if(SlideBtnState == MULTI_STATE_BTN_PLUS)
+		status = Rte_Call_rpSlideMotor_Move(MOTOR_STEP_PLUS);
+	
+	
+}
+
