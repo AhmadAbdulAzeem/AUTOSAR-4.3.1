503c4e617b9038e30b40571f434183ac12c27cc1
Ahmed AbdulAzeem <54190264+AhmadAbdulAzeem@users.noreply.github.com>
2020-09-08 21:58:00 +0200

    Add files via upload

---
+++ DIODriver/Dio_Cfg.h
@@ -0,0 +1,77 @@
+/**
+ * @file Dio_Cfg.h 
+ * @brief Configuration of Dio driver
+ *
+ * @author Ahmed AbdulAzeem 
+ * @date 4/9/2020
+ *
+ */
+
+#ifndef DIO_CFG_H
+#define DIO_CFG_H
+
+#include "Std_Types.h"
+
+#define PINS_NUMBER		(48u)
+#define PORTS_NUMBER   (6u)
+#define PINS_NUMBER_PER_PORT		(8U)
+
+#define DioFlipChannelApi STD_ON
+#define DioVersionInfoApi STD_OFF
+
+
+#define PA0						(0u)
+#define PA1						(1u)
+#define PA2						(2u)
+#define PA3						(3u)
+#define PA4						(4u)
+#define PA5						(5u)
+#define PA6						(6u)
+#define PA7						(7u)
+
+#define PB0						(8u)
+#define PB1						(9u)
+#define PB2						(10u)
+#define PB3						(11u)
+#define PB4						(12u)
+#define PB5						(13u)
+#define PB6						(14u)
+#define PB7						(15u)
+
+#define PC0						(16u)
+#define PC1						(17u)
+#define PC2						(18u)
+#define PC3						(19u)
+#define PC4						(20u)
+#define PC5						(21u)
+#define PC6						(22u)
+#define PC7						(23u)
+
+#define PD0						(24u)
+#define PD1						(25u)
+#define PD2						(26u)
+#define PD3						(27u)
+#define PD4						(28u)
+#define PD5						(29u)
+#define PD6						(30u)
+#define PD7						(31u)
+
+#define PE0						(32u)
+#define PE1						(33u)
+#define PE2						(34u)
+#define PE3						(35u)
+#define PE4						(36u)
+#define PE5						(37u)
+#define PE6						(38u)
+#define PE7						(39u)
+
+#define PF0						(40u)
+#define PF1						(41u)
+#define PF2						(42u)
+#define PF3						(43u)
+#define PF4						(44u)
+#define PF5						(45u)
+#define PF6						(46u)
+#define PF7						(47u)
+
+#endif
