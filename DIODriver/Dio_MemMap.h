12bab169da2361239104cd7bd0933b1d7905bae4
Ahmed AbdulAzeem <54190264+AhmadAbdulAzeem@users.noreply.github.com>
2020-09-08 21:58:22 +0200

    Add files via upload

---
+++ DIODriver/Dio_MemMap.h
@@ -0,0 +1,29 @@
+/**
+ * @file Dio_MemMap.h 
+ * @brief Registers addresses
+ *
+ * @author Ahmed AbdulAzeem 
+ * @date 4/9/2020
+ *
+ */
+
+#ifndef PORT_MEMMAP_H
+#define PORT_MEMMAP_H
+
+#define REG(R1, R2)							*((volatile uint32_t *)(R1 + R2))
+
+#define SYSCTL_RCGCGPIO_R       (*((volatile uint32_t *)0x400FE608))
+	
+#define GPIODATA_OFFSET		0x3FC
+#define GPIODIR_OFFSET 		0x400
+#define GPIOAFSEL_OFFSET	0x420
+#define GPIOPUR_OFFSET		0x510
+#define GPIOPDR_OFFSET		0x514
+#define GPIOSLR_OFFSET		0x518
+#define GPIODEN_OFFSET		0x51C
+#define GPIOLOCK_OFFSET		0x520
+#define GPIOAMSEL_OFFSET	0x528
+#define GPIOPCTL_OFSSET		0x52C
+#define GPIOICR_OFFSET		0x524
+
+#endif
