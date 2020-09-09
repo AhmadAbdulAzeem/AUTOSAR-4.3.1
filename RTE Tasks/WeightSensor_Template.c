8aba61482f70404458ae39ab292b949f8fb194ec
Ahmed AbdulAzeem <54190264+AhmadAbdulAzeem@users.noreply.github.com>
2020-09-09 07:26:35 +0200

    Add files via upload

---
+++ RTE Tasks/WeightSensor_Template.c
@@ -0,0 +1,30 @@
+/**
+ *
+ * \file WeightSensor_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: WeightSensor
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 9/4/2020 08:49 AM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_WeightSensor.h"
+
+
+/**
+ *
+ * Runnable WeightSensor_GetWeight
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppWeightSensor_GetWeight
+ *
+ */
+
+void WeightSensor_GetWeight (SensorWeightType* Weighr)
+{
+	Std_ReturnType status;
+
+}
+
