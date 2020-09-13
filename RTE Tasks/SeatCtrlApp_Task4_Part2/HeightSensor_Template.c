4e24f51a511613d45af37a87db9f8fafc17efe10
Ahmed AbdulAzeem <54190264+AhmadAbdulAzeem@users.noreply.github.com>
2020-09-13 15:21:46 +0200

    Add files via upload

---
+++ RTE Tasks/SeatCtrlApp_Task4_Part2/HeightSensor_Template.c
@@ -0,0 +1,34 @@
+/**
+ *
+ * \file HeightSensor_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: HeightSensor
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 9/13/2020 11:53 AM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_HeightSensor.h"
+
+
+/**
+ *
+ * Runnable HeightSensor_GetPosition
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppHeightSensor_GetPosition
+ *
+ */
+
+void HeightSensor_GetPosition (SensorPositionType* position)
+{
+	Std_ReturnType status;
+	IoPositionSensorReadingType position;
+
+	/* Server Call Points */
+	status = Rte_Call_rpIOGetHeight_IOGet(&position);
+	
+}
+
