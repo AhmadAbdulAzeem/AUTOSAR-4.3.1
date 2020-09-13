63c41e7ee5845f76d1d257394ceb699bee89f491
Ahmed AbdulAzeem <54190264+AhmadAbdulAzeem@users.noreply.github.com>
2020-09-13 15:21:16 +0200

    Create HeightMotor_Template.c

---
+++ RTE Tasks/SeatCtrlApp_Task4_Part2/HeightMotor_Template.c
@@ -0,0 +1,34 @@
+/**
+ *
+ * \file HeightMotor_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: HeightMotor
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 9/13/2020 11:53 AM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_HeightMotor.h"
+
+
+/**
+ *
+ * Runnable HeightMotor_Move
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppHeightMotor_Move
+ *
+ */
+
+void HeightMotor_Move (StepMotorStepType step)
+{
+	Std_ReturnType status;
+
+	/* Server Call Points */
+	status = Rte_Call_rpIOSetHeight_IOSetForward();
+	status = Rte_Call_rpIOSetHeight_IOSetReverse();
+	
+}
+
